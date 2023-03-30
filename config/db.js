7eadf38067e9fbdfc452ba734d604fee047b02cf
Sukhvinder Singh <sukhvindersingh@Sukhvinders-MacBook-Air.local>
2023-03-29 20:22:08 -0400

    check DB

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.URL)
+        console.log(process.env.URL);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
